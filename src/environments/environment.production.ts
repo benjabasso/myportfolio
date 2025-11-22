f163f2508e905b02024d810fc50a8608c33ccda4
Benjamin Basso <benjaminbasso7b@gmail.com>
2025-11-22 17:27:46 -0300

    Add Environments

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    emailjs: {
+        serviceId: 'service_bjiu3kj',
+        templateId: 'template_oyrp4qa',
+        publicKey: 'NSD9eHYHEv3fhjl8F'
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    emailJsPublicKey: 'NSD9eHYHEv3fhjl8F',
+    emailJsServiceId: 'service_bjiu3kj',
+    emailJsTemplateId: 'template_oyrp4qa'
+};
